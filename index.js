c36a34c77cdead0d5af77991ecc7ffb54bb09337
Steward <go778sgt@gmail.com>
2022-05-29 09:49:03 -0600

    socket fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const io = new Server(server);
 io.on('connection',socket => {
     console.log("User Connected: "+socket.id);
     socket.on('disconnect', reason => {
-        console.log("User Disconnected: "+ socket.id);
+        console.log("User Disconnected: "+ reason);
     });
     socket.on('req-update-videolist',msg => {
         socket.broadcast.emit('update-videolist');
